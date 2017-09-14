23f9282d3ce4e089cc7955dc6b7c567839815405
DanYellow <jeanlouis.danielo@sfr.fr>
2017-09-14 17:07:07 +0200

    Transforms into a stateless component Header component

--- src/js/containers/header.js
+++ src/js/containers/header.js
@@ -1,15 +1,14 @@
 import React from 'react'
 
-class Header extends React.Component {
-  render () {
-    return (
-      <div className="logo">
-        <a href="http://www.lesaventuresduchouchou.com/" target="_blank"><img src='assets/logo.png' /></a>
-        <br />
-        <span className="subtitle">{ this.props.subtitle }</span>
-      </div>
-    )
-  }
+
+const Header = () => {
+  return (
+    <div className="logo">
+      <a href="http://www.lesaventuresduchouchou.com/" target="_blank"><img src='assets/logo.png' /></a>
+      <br />
+      <span className="subtitle">{ this.props.subtitle }</span>
+    </div>
+  )
 }
 
 export default Header
